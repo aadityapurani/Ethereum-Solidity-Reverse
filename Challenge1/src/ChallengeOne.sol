0d8501b26d7cbf95ff6c94fdb4cbacc463ca79b0
Aaditya Purani <aaditya.purani@gmail.com>
2018-01-04 15:48:17 +0530

    Create ChallengeOne.sol

---
+++ Challenge1/src/ChallengeOne.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+
+/*
+Challenge One
+Author: Aaditya Purani
+*/
+
+contract ChallengeOne{
+    
+    string key  = "FLAG{R3V_S0L_1S_FUNN7}";
+    
+    function getKey() constant returns(string){
+        return key;
+    }
+}
